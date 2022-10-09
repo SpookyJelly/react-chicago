d66a7e5ef83a2228d22384895f76036b188ab722
SpookyJelly(Portable) <74237436+SpookyJelly@users.noreply.github.com>
2022-10-09 17:11:13 +0900

    1. page init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { Chicago } from "react-chicago";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <p>React-Chicago ShowCase</p>
+        <Chicago text="Hello, World!" color="white" cursorColor="white" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
