e3c5913bf5076b356345f56c0d70eefded1ce412
SpookyJelly <zang9412@gmail.com>
2022-10-14 15:14:19 +0900

    fix: gh-page 404 error

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: "./docs",
   build: {
-    outDir: "./docs",
+    outDir: ".",
   },
 });
