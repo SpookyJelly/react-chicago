d4cf33291eae993328916e864ac3834cab95abe2
SpookyJelly <zang9412@gmail.com>
2022-10-14 15:37:23 +0900

    3rd try) deploying gh-page on docs path

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: "./docs",
   build: {
-    outDir: ".",
+    outDir: "./docs",
   },
 });
