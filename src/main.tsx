d0fa383c07cb1cc11b77fd6eb38c28a93fe2cfbf
SpookyJelly <zang9412@gmail.com>
2022-10-21 23:12:34 +0900

    1. add env

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,19 @@ import English from "./pages/English";
 import Korean from "./pages/Korean";
 import "antd/dist/antd.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "en", element: <English /> },
-      { path: "kr", element: <Korean /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        { path: "en", element: <English /> },
+        { path: "kr", element: <Korean /> },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_NAME }
+);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <RouterProvider router={router} />
